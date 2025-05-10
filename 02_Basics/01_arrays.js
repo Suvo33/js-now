81a9a13130914519f8e83c745dad8bba3fdba713
Suvo33 <subhajitmanna2102@gmail.com>
2025-05-10 08:22:09 +0000

    arrays and jason concept

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,17 @@
+const myArray=[0,1,2,8]
+
+ 
+console.log(myArray[0])
+
+
+myArray.push(15)
+
+
+console.log(myArray);
+
+myArray.unshift(10)
+console.log(myArray.toString())
+
+
+
+
---
+++ 02_Basics/03_bobjects.js
@@ -0,0 +1,42 @@
+const tinderUser = new Object()
+
+// tinderUser.name='Subhajit Manna'
+// tinderUser.age =23
+
+// console.log(tinderUser);
+
+// const obj1={1:"a",2:"b"}
+// const obj2={3:"d",4:"g"}
+// const obj3={5:"c",6:"f"}
+
+// const obj4=Object.assign({},obj1,obj2,obj3)
+// console.log(obj4);
+
+// const obj5={...obj1,...obj2}
+// console.log(obj5);
+
+
+
+const container ={
+    name:"Subhajit Manna",
+    email:"Subhajit21manna@gmial.com",
+    mobNo:"968527419685",
+    courseId:"Udemy01"
+}
+
+// const {courseId}=container
+
+// console.log(courseId);
+
+const {courseId:id}=container
+
+
+console.log(id);
+
+
+
+
+
+
+
+
